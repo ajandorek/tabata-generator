6db07eccc73f6b4427bce88685dab51777d18490
Alec Jandorek <ajandorek@gmail.com>
2017-08-10 17:37:20 -0500

    updated server port

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 });
